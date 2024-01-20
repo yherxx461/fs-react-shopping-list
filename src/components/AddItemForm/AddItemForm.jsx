bde9738b9b2afe05c843ad33f0804a7ae7f47abb
Ying Her <yher.prime@gmail.com>
2024-01-20 13:07:05 -0600

    add break in the form input

--- src/components/AddItemForm/AddItemForm.jsx
+++ src/components/AddItemForm/AddItemForm.jsx
@@ -6,30 +6,31 @@ function AddItemForm (props) {
     const [quantityValue, setQuantityValue] = useState('');
     const [unitValue, setUnitValue] = useState('');
     
-    // handleChangeOfName - tracks what you type into the form
-    const handleChangeOfName = (event) => {
-        setNameValue(event.target.value);
-    };
-    // handleChangeOfQuantity - tracks what you type into the form
-    const handleChangeOfQuantity = (event) => {
-        setQuantityValue(event.target.value);
-    };
-    // handleChangeOfUnit - tracks what you type into the form
-    const handleChangeOfUnit = (event) => {
-        setUnitValue(event.target.value);
-    };
+    // // handleChangeOfName - tracks what you type into the form
+    // const handleChangeOfName = (event) => {
+    //     setNameValue(event.target.value);
+    // };
+    // // handleChangeOfQuantity - tracks what you type into the form
+    // const handleChangeOfQuantity = (event) => {
+    //     setQuantityValue(event.target.value);
+    // };
+    // // handleChangeOfUnit - tracks what you type into the form
+    // const handleChangeOfUnit = (event) => {
+    //     setUnitValue(event.target.value);
+    // };
+    
     const handleSubmitShoppingList= (event) => {
         event.preventDefault(); // stops the page from refreshing when submit button is pressed
         console.log ('Values for submit: ', {
             name: nameValue,
             quantity: quantityValue,
-            unit: unitValue
+            unit: unitValue,
         });
     
         //post data
         postItems({
-            name: nameValue,
-            quantity: quantityValue,
+            name: nameValue, 
+            quantity: Number(quantityValue),
             unit: unitValue,
         })
         .then((response) => {
@@ -37,7 +38,7 @@ function AddItemForm (props) {
             
             //clear
             setNameValue('');
-            setQuantity('');
+            setQuantityValue('');
             setUnitValue('');
         })
         .catch((err) => {
@@ -47,12 +48,16 @@ function AddItemForm (props) {
 
     return(
         <div>
-            <form onSubmit={handleSubmitShoppingList}>
-            <h1>Add an Item</h1>
-            Item:<input type="text" onChange={handleChangeOfName} value={nameValue}></input> 
-            Quantity:<input type="text" onChange={handleChangeOfQuantity} value={quantityValue}></input>
-            Unit:<input type="text" onChange={handleChangeOfUnit} value={unitValue}></input>
-            <button >Save</button>
+            <form className="container">
+                <h1>Add an Item</h1>
+                {/* Item:<input type="text" onChange={handleChangeOfName} value={nameValue}></input>  */}
+                Item:<input onChange={(event) => setNameValue(event.target.value)} value={nameValue}></input>
+                <br></br>
+                {/* Quantity:<input type="text" onChange={handleChangeOfQuantity} value={quantityValue}></input> */}
+                Quantity:<input onChange={(event) => setQuantityValue(event.target.value)} value={quantityValue}></input>
+                {/* Unit:<input type="text" onChange={handleChangeOfUnit} value={unitValue}></input> */}
+                Unit:<input onChange={(event) => setUnitValue(event.target.value)} value={unitValue}></input>
+                <button onClick={handleSubmitShoppingList}>Save</button>
             </form>
         </div>
     )
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect }  from 'react';
-import React from 'react';
+// import react from 'react';
 
 import AddItemForm from '../AddItemForm/AddItemForm.jsx';
 import {fetchItems} from '../AddItemApi/addItem.api.js';
@@ -10,7 +10,7 @@ import './App.css';
 function App() {
     // declare our variable
     const [shoppingList, setShoppingList] = useState([
-        {name: "milk", quantity: 1, unit: "gallon"}
+        // {name: "milk", quantity: 1, unit: "gallon"}
     ]);
 
     //Refresh Shopping lists
@@ -44,11 +44,12 @@ function App() {
             <AddItemForm listRefreshCallback = {refreshShoppingList}/>
             <main>
                 {/* render shopping list onto page */}
+            <h1>Shopping List</h1>
                 {shoppingList.map((itemData, dataIndex) => {
                     return (
                         <div key={dataIndex}>
                             {/* How we want the data to appear */}
-                            <h3>{itemData.name}</h3>
+                            <p>Item: {itemData.name}</p>
                             <p>Quantity: {itemData.quantity} Unit: {itemData.unit} </p>
                             <button type="button">Purchased</button>
                             <button type="button">Remove</button>
